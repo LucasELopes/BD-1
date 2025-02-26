314592ccf3e0e8d68876cd68380af1959aadf479
nathan <nathanmont00@gmail.com>
2025-02-26 17:04:09 -0300

    Migration criada (tabela morador)

---
+++ backend/database/migrations/2025_02_26_180226_morador.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('morador', function (Blueprint $table){
+
+            $table->char('cpfMorador', 11);
+            $table->char('nmrSUS', 15);
+            $table->string('nomeMorador', 100);
+            $table->string('nomeMae', 100);
+            $table->date('dataNascimento');
+            $table->char('sexo', 1);
+            $table->text('endereco');
+            $table->string('estadoCivil', 20);
+            $table->string('escolaridade', 50);
+            $table->string('etnia', 50);
+            $table->boolean('planoSaude')->default(false);
+
+            $table->primary('cpfMorador');
+
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('morador');
+    }
+};
